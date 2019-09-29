66a89eb735ddf5798c1c92231fd9e2761fddc042
Hamdy Alio <hamdialio84@gmail.com>
2019-09-29 02:13:18 +0200

    Neue Projekt MessagePrinter wurde hinzugefügt

--- MessagePrinter/Class1.cs
+++ MessagePrinter/Class1.cs
@@ -4,13 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SimpleDependency
+namespace MessagePrinter
 {
-    class Program
+    public class Class1
     {
-        static void Main(string[] args)
-        {
-
-        }
     }
 }
